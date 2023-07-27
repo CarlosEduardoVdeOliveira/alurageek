d2ec116657b4eacd21c0a86a51e3b3d2607dbb9f
Carlos Eduardo <vieiradeoliveira70@gmail.com>
2023-07-27 16:18:19 -0300

    bug link

--- js/sessionStorage.js
+++ js/sessionStorage.js
@@ -11,7 +11,7 @@ formLogin.addEventListener("submit", (event) => {
     password.value.length >= 6 &&
     password.value !== ""
   ) {
-    window.location.href = "../alurageek/pages/home-logged.html";
+    window.location.href = "../pages/home-logged.html";
     sessionStorage.setItem("email", email.value);
     sessionStorage.setItem("password", password.value);
   } else {
